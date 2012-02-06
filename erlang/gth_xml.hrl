7d0b9d5bf4f032b315cea4d227772f56f84739a1
Matthias Lang <matthias@corelatus.com>
2012-02-06 12:16:05 +0100

    Clarify comment

--- erlang/gth_xml.hrl
+++ erlang/gth_xml.hrl
@@ -1,12 +1,26 @@
 %%----------------------------------------------------------------------
-%% Title: record definitions for the GTH API. 
+%% Title: Record which represents an XML tree
 %% Author: Matthias (matthias@corelatus.se)
 %%
-%% Purpose: All GTH-related records which are passed between API modules are
-%%          here, including the ones used by gth_client_xml_parse.erl
+%% For the GTH's purposes, any XML can be represented as a tree, with
+%% each node in the tree having:
+%%
+%%   - a name
+%%   - zero or more attributes
+%%   - zero or more children (each of which is an XML tree)
+%%   - possibly, some text ("clippings")
+%%
+%% Example:
+%%
+%%    <a><b p='q'/></a>
+%%
+%%   is equivalent to
 %% 
+%%    #resp_tuple{name=a, attributes=[], clippings=[],
+%%                children=[#resp_tuple{name=b, attributes=[{"p", "q"}]}]}
+%%
+%% Whenever the GTH parses XML, it returns a #resp_tuple.
 
-%% Responses from the GTH. A general XML tuple.
 -record(resp_tuple, {
 	  name            :: atom(),             % e.g. 'ok' | 'error' | ...
 	  attributes = [] :: [{Key::string(), Value::string()}],
